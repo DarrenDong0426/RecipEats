e47c7cc225ffe61e6b28aa29aab005225f05643a
0426DarrenDong <79467048+DarrenDong0426@users.noreply.github.com>
2022-03-20 20:05:16 -0400

    Create text_constants.dart

---
+++ lib/core/const/text_constants.dart
@@ -0,0 +1,18 @@
+// TAGS
+
+const String Fast = 'Fast';
+const String Healthy = 'Healthy';
+const String Dessert = 'Dessert';
+const String Meal = 'Meal';
+const String Snack = 'Snack';
+const String Soup = 'Soup';
+const String Chinese = 'Chinese';
+const String Japanese = 'Japanese';
+const String Indian = 'Indian';
+const String Mexican = 'Mexican';
+const String Simple = 'Simple';
+
+// Filters
+
+const String Calories = 'Calories';
+const String PrepTime = 'Prep Time';

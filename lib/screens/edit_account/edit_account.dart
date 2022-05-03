e43be8e8b0c3b724cbbb1ce68750b6dd490d9e02
DarrenDong0426 <79467048+DarrenDong0426@users.noreply.github.com>
2022-05-03 09:03:17 -0400

    Add files via upload

--- lib/screens/edit_account/edit_account.dart
+++ lib/screens/edit_account/edit_account.dart
@@ -1,1 +1,10 @@
-const data = null;
+import 'package:flutter/cupertino.dart';
+
+class Edit_Account extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
--- lib/screens/search_recipes/search_recipe.dart
+++ lib/screens/search_recipes/search_recipe.dart
@@ -1,1 +1,42 @@
-const data = null;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:recipeats/data/recipes.dart';
+import 'package:recipeats/screens/home_screen/home_screen.dart';
+
+class Search_Recipes extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+    return Scaffold(
+      //appBar: AppBar(title: Text("Recordings"),),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: recipes.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SizedBox(
+                    height: 100,
+                    child: Row(
+                      children: <Widget>[
+                        Text("data"),
+                      ],
+                    ),
+                  );
+                }
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+}

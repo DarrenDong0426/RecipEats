b1f56ef656aabfa64b099593b6bd4a944fd177b0
DarrenDong0426 <79467048+DarrenDong0426@users.noreply.github.com>
2022-05-03 09:21:43 -0400

    Add files via upload

---
+++ lib/utils/const/text_constants.dart
@@ -0,0 +1,19 @@
+// TAGS (maximum 5 per food)
+
+const String Fast = 'Fast';
+const String Healthy = 'Healthy';
+const String Dessert = 'Dessert';
+const String Meal = 'Meal';
+const String Snack = 'Snack';
+const String Soup = 'Soup';
+const String Chinese = 'Chinese';
+const String Japanese = 'Japanese';
+const String Indian = 'Indian';
+const String Mexican = 'Mexican';
+const String Simple = 'Simple';
+const String Korean = 'Korean';
+
+// Filters
+
+const String Calories = 'Calories';
+const String PrepTime = 'Prep Time';

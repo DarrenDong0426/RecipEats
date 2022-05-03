c46bca02cec147cfc87b1ad7879fac4f450531e8
0426DarrenDong <79467048+DarrenDong0426@users.noreply.github.com>
2022-05-03 08:58:12 -0400

    Vor

---
+++ lib/data/firebase/info.dart
@@ -0,0 +1,18 @@
+String email = '';
+String password = '';
+
+setEmail(String s){
+  email = s;
+}
+
+setPassword(String s){
+  password = s;
+}
+
+getEmail(String s){
+  return email;
+}
+
+getPassword(String s){
+  return password;
+}
